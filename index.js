77f3e34f2cddd6b4f15c11e2ee435d79d66213f0
Nuwan-alt <jayasankanuwan1998@gmail.com>
2022-11-12 00:43:16 +0530

    line deleted

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const { WebSocketServer } = require("ws") ;
-const server = new WebSocketServer({ port: 3000 });
+
 
 //databse connection
 // require('./configs/db')();
