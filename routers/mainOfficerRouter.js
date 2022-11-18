4d73c3f42e88cba32a25892592ef0d3197db88aa
Piyumi Chan Mahaarachchi <76487449+YumeChaan@users.noreply.github.com>
2022-11-18 21:01:34 +0530

    edit gso

--- routers/mainOfficerRouter.js
+++ routers/mainOfficerRouter.js
@@ -4,7 +4,9 @@ const router = express.Router();
 
 router.post('/register-gso', gsoController.gsoRegister); //register gso 
 router.get('/get-gso/:nic', gsoController.getGsoDetails); //get gso details
-router.delete('/remove-gso/:id', gsoController.removeGso); //delete gso
+router.delete('/remove-gso', gsoController.removeGso); //delete gso
 router.post('/check-availability-gso',gsoController.checkAvailabilityGSO); //check availability of gso
+router.post('/user-details', gsoController.getUserDetailsGso); //get user details
+router.put('/edit-gso', gsoController.editGSO); //edit gso details
 
 module.exports = router;
