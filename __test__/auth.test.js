d4e183a0be44fd961eed59efaa1e4e18d55dcc83
SumeelaMadusankha <hasumeela@gmail.com>
2022-11-20 22:41:39 +0530

    test

---
+++ __test__/auth.test.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const app = require('../server')
+const request = require('supertest')
+
+
+beforeAll((done) => {
+    mongoose.connect("mongodb+srv://farm-to-mart:Bh1eDPKbuFTGnXm8@cluster0.knoo6tu.mongodb.net/FarmToMartMock",
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        () => done());
+});
+
+describe("Farmer Regiter", () => {
+    describe("given a nic", () => {
+        test("should responce with 201 code", async() => {
+            // const request = require('supertest')
+            const responce = await request(app).post("/api/farmer/register").send({
+                "firstName":"Sumeela",
+                "lastName":"Madusankha",
+                "address":"Kotapola,Deniyaya",
+                "phone":"0766929289",
+                "district":"Matara",
+                "gsdName":"Usamalagoda",
+                "gsdCode":"123",
+                "email":"asumela@gmail.com",
+                "nic":"990910230V",
+                "password":"@Farmer123"
+            })
+            expect(responce.statusCode).toBe(200)
+        })
+
+    })
+
+    describe("not given anic", () => {
+        test("should responce with 201 code", async() => {
+            // const request = require('supertest')
+            const responce = await request(app).post("/api/farmer/register").send({
+                "firstName":"Sumeela",
+                "lastName":"Madusankha",
+                "address":"Kotapola,Deniyaya",
+                "phone":"0766929289",
+                "district":"Matara",
+                "gsdName":"Usamalagoda",
+                "gsdCode":"123",
+                "email":"asumela@gmail.com",
+                "nic":"990910230V",
+                "password":"@Farmer123"
+            })
+            expect(responce.statusCode).toBe(200)
+        })
+
+    })
+
+
+})
